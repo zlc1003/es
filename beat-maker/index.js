63946ec8c262ce36398db504015c5a7f442eac36
zlc1003 <649536862@qq.com>
2022-05-09 09:53:59 -0700

    asdfgfdfgfdfghgfdgfdfghgfdfgfdfg

--- beat-maker/index.js
+++ beat-maker/index.js
@@ -1,24 +1,34 @@
 var cboxnum = 0
 var printdiv = document.getElementById("print")
-function hmok() {
-    cboxnum = document.getElementById("howmuch").value
-    document.getElementById("hmdiv").style.display = "none"
-    main()
-}
+var print2div = document.getElementById("print2")
 function p(t) {
     console.log(t)
 }
 function write(t) {
-    printdiv.innerHTML += t
+    printdiv.innerHTML = t
 }
+function write2(t) {
+    print2div.innerHTML += t
+}
+// window.onresize = function () {
+//     kuan = document.body.clientWidth/2
+//     gao = document.body.clientHeight/2.8
+//     let cn=((kuan/13)*(gao/13))
+//     cbox = "<input type=\"checkbox\" class=\"checkboxhahaha\" onclick=\"gen()\"></input>"
+//     for (let i = 0; i < cn; i++) {
+//         inhtml += cbox
+//     }
+//     write(inhtml)
+// }
 function main() {
+    document.getElementById("hmdiv").style.display = "none"
     inhtml = ''
     cbox = "<input type=\"checkbox\" class=\"checkboxhahaha\" onclick=\"gen()\"></input>"
     for (let i = 0; i < cboxnum; i++) {
         inhtml += cbox
     }
     write(inhtml)
-    write(`<br><textarea id="output" cols="30" rows="10" readonly unselectable="on"></textarea>`)
+    write2(`<br><textarea id="output" cols="30" rows="10" readonly unselectable="on" style="width: 100%;height: 100%;display=none;"></textarea>`)
     gen()
 }
 function gen() {
@@ -35,3 +45,15 @@ function gen() {
     }
     output.value = out
 }
+main();
+(function () {
+    kuan = document.body.clientWidth/2
+    gao = document.body.clientHeight/2.8
+    let cn=((kuan/13)*(gao/13))
+    cbox = "<input type=\"checkbox\" class=\"checkboxhahaha\" onclick=\"gen()\"></input>"
+    for (let i = 0; i < cn; i++) {
+        inhtml += cbox
+    }
+    write(inhtml)
+})();
+gen()
